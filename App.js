d6a8201eafda60c224583cf9cb5a6ccf309944a0
DSMac <mohd.omerquadri@dsmartsys.in>
2020-11-30 00:36:01 +0530

    Format code

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import React from "react"
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-import LoginScreen from "./components/LoginPage";
-import EmailScreen from "./components/EmailPage";
-import EmailMain from "./components/MainEmails";
-import ContentPage from "./components/ContentPage"
-import TermsScreen from "./components/TermsScreen"
+import LoginScreen from './components/LoginPage';
+import EmailScreen from './components/EmailPage';
+import EmailMain from './components/MainEmails';
+import ContentPage from './components/ContentPage';
+import TermsScreen from './components/TermsScreen';
 
 const Stack = createStackNavigator();
 
@@ -14,66 +14,63 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-	      initialRouteName={'Login'}
+        initialRouteName={'Login'}
         screenOptions={{
           gestureEnabled: true,
           gestureDirection: 'vertical',
           animationEnabled: false,
         }}
         mode={'card'}>
-
-      <Stack.Screen
+        <Stack.Screen
           name="Terms"
-          component={TermsScreen} 
-            options={{
-              headerShown:false
-            }}
-          />
+          component={TermsScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
 
-      <Stack.Screen 
+        <Stack.Screen
           name="Login"
-          component={LoginScreen} 
-            options={{
-              headerShown:false
-            }}
-          />
-      
+          component={LoginScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
 
-      <Stack.Screen 
+        <Stack.Screen
           name="Email"
-          component={EmailScreen} 
-            options={{
-              headerShown:false
-            }}
-          />  
-          
-      <Stack.Screen 
+          component={EmailScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+
+        <Stack.Screen
           name="Content"
-          component={ContentPage} 
-            options={{
-                title: 'Back',
-                headerStyle: {
-                  backgroundColor: '#1A1A1F',
-                  shadowColor: 'transparent'
-                },
-                headerTintColor: '#FFF'
-              }}
-          />   
-       
-      <Stack.Screen 
+          component={ContentPage}
+          options={{
+            title: 'Back',
+            headerStyle: {
+              backgroundColor: '#1A1A1F',
+              shadowColor: 'transparent',
+            },
+            headerTintColor: '#FFF',
+          }}
+        />
+
+        <Stack.Screen
           name="EmailNow"
-          component={EmailMain} 
-            options={{
-                title: 'All Emails',
-                headerStyle: {
-                  backgroundColor: '#1A1A1F',
-                  shadowColor: 'transparent'
-                },
-                headerTintColor: '#FFF'
-              }}
-          />
+          component={EmailMain}
+          options={{
+            title: 'All Emails',
+            headerStyle: {
+              backgroundColor: '#1A1A1F',
+              shadowColor: 'transparent',
+            },
+            headerTintColor: '#FFF',
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
